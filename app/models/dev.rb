5610e74a7711fb0ebe559bf17a5b78c987f9b042
Patrick Mason <patrickmason73@gmail.com>
2023-06-01 01:07:47 -0600

    commit tree dbaefc6dadfeeaac2df1f004955d9b3ecdd73cae

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,16 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
 
+    def received_one?(item_name)
+        self.freebies.any? do |freebie|
+            freebie.item_name == item_name
+        end
+    end
+
+    def give_away(dev, freebie)
+        if self.id == freebie.dev_id
+        freebie.update(dev: dev)
+        end
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,19 @@
+Company.delete_all
+Dev.delete_all
+Freebie.delete_all
+
+
 puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
-Company.create(name: "Facebook", founding_year: 2004)
-Company.create(name: "Dunder Mifflin", founding_year: 2002)
-Company.create(name: "Enron", founding_year: 1995)
+google = Company.create(name: "Google", founding_year: 1998)
+facebook = Company.create(name: "Facebook", founding_year: 2004)
+office = Company.create(name: "Dunder Mifflin", founding_year: 2002)
+enron = Company.create(name: "Enron", founding_year: 1995)
 
 puts "Creating devs..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
-Dev.create(name: "Mr. Meseeks")
-Dev.create(name: "Gazorpazop")
+rick = Dev.create(name: "Rick")
+morty = Dev.create(name: "Morty")
+meseeks = Dev.create(name: "Mr. Meseeks")
+gazorpazop = Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
 
@@ -17,5 +22,9 @@ puts "Creating freebies..."
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
 # Create freebies Here
+
+Freebie.create(item_name: "grungle", value: 10, dev_id: rick.id, company_id: google.id)
+Freebie.create(item_name: "bungler", value: 100, dev_id: morty.id, company_id: facebook.id)
+Freebie.create(item_name: "grungle", value: 1000, dev_id: meseeks.id, company_id: office.id)
 
 puts "Seeding done!"
